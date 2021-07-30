ddc7c46e7c22632b4ad1d094b1d43fdb939ae474
renanwurster <totozerameister@gmail.com>
2021-07-30 17:53:29 +0200

    initial

---
+++ app/src/main/java/com/example/android/navigation/AboutFragment.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2018, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.navigation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.findBinding
+import androidx.databinding.DataBindingUtil.setContentView
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.android.navigation.databinding.ActivityMainBinding
+import com.example.android.navigation.databinding.FragmentAboutBinding
+import com.example.android.navigation.databinding.FragmentGameBinding
+import com.example.android.navigation.databinding.FragmentTitleBinding
+
+
+
+
+
+class AboutFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = DataBindingUtil.inflate<FragmentAboutBinding>(
+            inflater, R.layout.fragment_about, container, false
+        )
+
+        binding.playButton.setOnClickListener { view: View ->
+            view.findNavController().navigate(AboutFragmentDirections.actionAboutFragmentToGameFragment())
+            Toast.makeText(context, "Renan is cool :)", Toast.LENGTH_SHORT ).show()
+            Toast.makeText(context, "Good luck :>!", Toast.LENGTH_SHORT ).show()
+        }
+        return binding.root
+    }
+}
